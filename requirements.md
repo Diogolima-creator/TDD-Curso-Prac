63ba80849d0c3c277a0953518d3a22079b1d71c3
Diogolima-creator <dbznetudo@gmail.com>
2022-04-02 22:38:45 -0400

    add requirements.md

---
+++ requirements.md
@@ -0,0 +1,5 @@
+1. Criar o createUser passando email e pass 
+2. Criar o loginUser passando email e pass
+3. Criar exceção ao erro em teste log/reg
+4. Criar validation para log/reg
+5. integrar ao express
--- src/data/useCases/create-user/create-user.spec.ts
+++ src/data/useCases/create-user/create-user.spec.ts
@@ -1,12 +1,9 @@
 import { CreateUser } from '@/data/useCases'
 import { mockUser } from '@/data/tests'
 
-type SutTypes = {
-  sut: CreateUser
-}
 
 describe('CreateUser',() => {
- test('Should return a empty list on sut.createUser', () => {
+ test('Should user created on sut.createUser', () => {
     const sut = new CreateUser()
     const promise = sut.createUser(mockUser())
     expect(promise).toEqual(Object(promise))
