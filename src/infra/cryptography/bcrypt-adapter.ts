d1a5720307aad6ecba3daaa859e642a46af82353
Diogolima-creator <dbznetudo@gmail.com>
2022-04-10 18:16:50 -0400

    add bcryptAdapter and create functions hash and compare

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,3 +1,15 @@
-import { Hasher, HashCompare } from '@/data/protocols'
+import { Hasher, HashComparer } from '@/data/protocols'
+import bcrypt from 'bcrypt'
 
-import bcrypt from ''
+export class BcryptAdapter implements Hasher, HashComparer{
+   constructor(private readonly salt:number){}
+
+   async hash(plaintext: string): Promise<string>{
+      return bcrypt.hash(plaintext, this.salt)
+   }
+
+   async compare(plaitext: string, digest: string):Promise<boolean>{
+      return bcrypt.compare(plaitext, digest)
+   }
+
+}
