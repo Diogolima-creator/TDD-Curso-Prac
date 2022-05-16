5405a08cce54cc011681eb600737d7a424c7be0c
Diogolima-creator <dbznetudo@gmail.com>
2022-05-16 10:00:20 -0400

    finish routes in config to setupRoutes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -6,6 +6,6 @@ import express, { Express } from 'express'
 export const setupApp = async(): Promise<Express> => { 
   const app = express()
   setupMiddlewares(app)
-
+  setupRoutes(app)
   return app
 }
