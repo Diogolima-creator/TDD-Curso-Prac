37c187814e579fa63c48108b4068ae47dae75e2e
Diogolima-creator <dbznetudo@gmail.com>
2022-05-16 12:59:20 -0400

    create validation: compare-fields

---
+++ src/main/factories/controllers/signup-validation-factory.ts
@@ -0,0 +1,3 @@
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+import { EmailValidatorAdapter } from '@/infra/validators'
