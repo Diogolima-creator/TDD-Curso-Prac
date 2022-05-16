59e3d5814ce5e1a362362b4d521f82f0211d98ac
Diogolima-creator <dbznetudo@gmail.com>
2022-05-16 10:34:16 -0400

    finish login-controller

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,3 +1,33 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { badRequest, serverError, unauthorized, ok } from '@/presentation/helpers'
 import { loadUser } from '@/domain'
+
+export class LoginController implements Controller {
+  constructor(
+    private readonly loaduser: loadUser,
+    private readonly validation: Validation
+  ){}
+
+  async handle(request: LoginController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request)
+      if(error){
+        return badRequest(error)
+      }
+      const authenticationModel = await this.loaduser.auth(request)
+      if (!authenticationModel){
+        return unauthorized()
+      }
+      return ok(authenticationModel)
+    } catch(error){
+      return serverError(error)
+    }
+  }
+}
+
+export namespace LoginController {
+  export type Request = {
+    email: string
+    password: string
+  }
+}
