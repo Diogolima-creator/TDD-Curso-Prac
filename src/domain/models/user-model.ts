3053cdf03d22bb94ca3a0fc8cf56d3c3ea6aa7f9
Diogolima-creator <dbznetudo@gmail.com>
2022-05-21 13:13:21 -0400

    update the userModel with params username,profilepic,level,description,classes and last classes

--- src/domain/models/user-model.ts
+++ src/domain/models/user-model.ts
@@ -2,4 +2,10 @@ export interface UserModel {
     id?: string
     email: string
     password: string
+    username: string
+    profilePic: string
+    level: string
+    description: 'Atualizar quando entrar'
+    classes: ['1.JSTutorial','Statements','9fhKtle7Ivg']
+    LastClasses: [0,-1]
 } 
