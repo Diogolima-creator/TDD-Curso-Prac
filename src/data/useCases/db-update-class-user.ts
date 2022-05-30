de04528a0b7c9cf69ccc89060cc1d51d3dfb2622
Diogolima-creator <dbznetudo@gmail.com>
2022-05-30 12:25:33 -0400

    create db-update-class-user

---
+++ src/data/useCases/db-update-class-user.ts
@@ -0,0 +1,12 @@
+import { updateClassUser } from "@/domain/useCases/update-class-user"
+import { UpdateClassUserRepository } from "@/data/protocols"
+import { UserModel } from "@/domain/models"
+
+
+export class DbUpdateClassUser implements updateClassUser {
+  constructor(private readonly updateClassUserRepository: UpdateClassUserRepository){}
+
+  async update (updateDate: UserModel): Promise<void> {
+    return this.updateClassUserRepository.updateClass(updateDate.id,updateDate.classes[0],updateDate.classes[1], updateDate.classes[2])
+  }
+}
