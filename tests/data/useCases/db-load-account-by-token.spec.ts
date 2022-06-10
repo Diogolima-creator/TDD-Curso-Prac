dde2961748e21e7135b7f1b5250d24e3a5e9e3af
Diogolima-creator <dbznetudo@gmail.com>
2022-06-10 17:02:09 -0400

    create makeSut in db-load-account-by-token to starting creation the tests

--- tests/data/useCases/db-load-account-by-token.spec.ts
+++ tests/data/useCases/db-load-account-by-token.spec.ts
@@ -1,3 +1,28 @@
 import { DbLoadAccountByToken } from "@/data/useCases"
 import { mockUser, throwError } from "@/tests/domain/mocks"
-import { DecrypterSpy } from '@/tests/data/mocks'
+import { DecrypterSpy, LoadAccountByTokenRepositorySpy } from '@/tests/data/mocks'
+
+type SutTypes = {
+  sut: DbLoadAccountByToken
+  decrypterSpy: DecrypterSpy
+  loadAccountByTokenRepositorySpy: LoadAccountByTokenRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterSpy = new DecrypterSpy()
+  const loadAccountByTokenRepositorySpy = new LoadAccountByTokenRepositorySpy()
+  const sut = new DbLoadAccountByToken(decrypterSpy, loadAccountByTokenRepositorySpy)
+
+  return {
+    sut,
+    decrypterSpy,
+    loadAccountByTokenRepositorySpy
+  }
+}
+
+let token: string 
+let role : string 
+
+describe('DbLoadAccountToken UseCase', () => {
+  
+})
