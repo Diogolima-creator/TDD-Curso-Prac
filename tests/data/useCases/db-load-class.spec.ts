7adf88105918549237c7c6d62252b0f269da973c
Diogolima-creator <dbznetudo@gmail.com>
2022-06-10 18:13:20 -0400

    create makeSut in db-load-class

--- tests/data/useCases/db-load-class.spec.ts
+++ tests/data/useCases/db-load-class.spec.ts
@@ -1,3 +1,23 @@
 import { DbLoadClass } from "@/data/useCases"
-import { LoadClassByTypeRepositorySpy } from "@/data/mocks"
-import { mockUser, throwError } from '@/tests/domain/mocks'
+import { LoadClassByTypeRepositorySpy } from "@/tests/data/mocks"
+import { mockUser, throwError } from "@/tests/domain/mocks"
+
+type SutTypes = {
+  sut: DbLoadClass,
+  loadClassByTypeRepositorySpy: LoadClassByTypeRepositorySpy
+}
+
+const makeSut = ():SutTypes => {
+
+  const loadClassByTypeRepositorySpy = new LoadClassByTypeRepositorySpy()
+  const sut = new DbLoadClass(
+    loadClassByTypeRepositorySpy
+  )
+
+  return {
+    sut,
+    loadClassByTypeRepositorySpy
+  }
+
+  
+}
