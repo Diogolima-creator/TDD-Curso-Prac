dc65252dbe297ebad85045857fe8e6ef0011bf9b
Diogolima-creator <dbznetudo@gmail.com>
2022-06-11 11:38:19 -0400

    create MakeSut in db-update-class-user

--- tests/data/useCases/db-update-class-user.spec.ts
+++ tests/data/useCases/db-update-class-user.spec.ts
@@ -1,1 +1,19 @@
 import { DbUpdateClassUser } from "@/data/useCases"
+import { UpdateClassUserRepositorySpy } from "@/tests/data/mocks"
+import { throwError } from '@/tests/domain/mocks'
+
+type SutTypes = {
+  sut: DbUpdateClassUser
+  updateClassUserRepositorySpy: UpdateClassUserRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const updateClassUserRepositorySpy = new UpdateClassUserRepositorySpy()
+  const sut = new DbUpdateClassUser(
+    updateClassUserRepositorySpy
+  )
+  return {
+    sut,
+    updateClassUserRepositorySpy
+  }
+}
