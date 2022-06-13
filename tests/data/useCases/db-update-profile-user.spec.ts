8881a1dcb7518c8c730c93749cdb5e3eabe9e1eb
Diogolima-creator <dbznetudo@gmail.com>
2022-06-13 01:39:12 -0400

    create test: Should throw if updateProfileRepository throws

--- tests/data/useCases/db-update-profile-user.spec.ts
+++ tests/data/useCases/db-update-profile-user.spec.ts
@@ -22,11 +22,18 @@ const makeSut = (): SutTypes => {
 
 describe('DbUpdateProfile useCase', () => {
 
-  test('Should Call updateProfileRepositorySpy', async () => {
+  test('Should Call UpdateProfileRepository', async () => {
     const { sut, updateProfileRepositorySpy } = makeSut()
     await sut.update({id:'id', description: 'description', profilePic: 'profilePic'})
     expect(updateProfileRepositorySpy.id).toBe('id')
     expect(updateProfileRepositorySpy.description).toBe('description')
     expect(updateProfileRepositorySpy.profilePic).toBe('profilePic')
+  })
+
+  test('Should throw if UpdateProfileRepository throws', async () => {
+    const { sut, updateProfileRepositorySpy } = makeSut()
+    jest.spyOn(updateProfileRepositorySpy, 'updateProfile').mockImplementationOnce(throwError)
+      const promise = sut.update({id:'id', description: 'description', profilePic: 'profilePic'})
+      await expect(promise).rejects.toThrow()
   })
 })
