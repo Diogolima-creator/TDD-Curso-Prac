b035344723e6d5a6f812253d6474fc71074fa898
Diogolima-creator <dbznetudo@gmail.com>
2022-08-01 01:34:34 -0400

    finish in presentation get and create posts controllers

---
+++ src/presentation/controllers/get-post-controller.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { noContent, serverError, ok } from '@/presentation/helpers'
+import { GetPost } from '@/domain'
+
+export class GetPostController implements Controller {
+  constructor (private readonly getPost: GetPost){}
+
+  async handle (): Promise<HttpResponse> {
+    try{
+      const posts = await this.getPost.getAll()
+      return posts.id ? ok(posts) : noContent()
+    }catch(error){
+      return serverError(error)
+    }
+  }
+}
---
+++ src/presentation/controllers/post-create-controller.ts
@@ -0,0 +1,22 @@
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { serverError, ok } from '@/presentation/helpers'
+import { CreatePost } from '@/domain'
+
+export class CreatePostController implements Controller {
+  constructor(
+    private readonly createPost: CreatePost
+  ){}
+
+  async handle(request: CreatePostController.Request): Promise<HttpResponse>{
+    try{
+      const postModel = await this.createPost.create(request)
+      return ok(postModel)
+    } catch(error){
+      return serverError(error)
+    }
+  }
+}
+
+export namespace CreatePostController {
+  export type Request = CreatePost.Params
+}
